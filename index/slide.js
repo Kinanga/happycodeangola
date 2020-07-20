78d50dbadcb4a2a10f625d84358f4a18b140ecd7
Kinanga <kinangamjoao@gmail.com>
2020-07-20 12:36:01 +0100

    versão 0.1

---
+++ index/slide.js
@@ -0,0 +1,74 @@
+var intervalo = 3000; //troca a imagem a cada 3 segundos
+
+function slide1(){
+    document.getElementById("banner").src="img/S1.JPG"
+    setTimeout("slide2()",intervalo);
+}
+function slide2(){
+    document.getElementById("banner").src="img/S2.JPG"
+    setTimeout("slide3()",intervalo);
+}
+function slide3(){
+    document.getElementById("banner").src="img/S3.JPG"
+    setTimeout("slide4()",intervalo);
+}
+function slide4(){
+    document.getElementById("banner").src="img/S4.JPG"
+    setTimeout("slide5()",intervalo);
+}
+function slide5(){
+    document.getElementById("banner").src="img/S5.JPG"
+    setTimeout("slide6()",intervalo);
+}
+function slide6(){
+    document.getElementById("banner").src="img/S6.JPG"
+    setTimeout("slide7()",intervalo);
+}
+function slide7(){
+    document.getElementById("banner").src="img/S7.JPG"
+    setTimeout("slide8()",intervalo);
+}
+function slide8(){
+    document.getElementById("banner").src="img/S8.JPG"
+    setTimeout("slide9()",intervalo);
+}
+function slide9(){
+    document.getElementById("banner").src="img/S9.JPG"
+    setTimeout("slide10()",intervalo);
+}
+function slide10(){
+    document.getElementById("banner").src="img/S10.JPG"
+    setTimeout("slide11()",intervalo);
+}
+function slide11(){
+    document.getElementById("banner").src="img/S11.JPG"
+    setTimeout("slide12()",intervalo);
+}
+function slide12(){
+    document.getElementById("banner").src="img/S12.JPG"
+    setTimeout("slide13()",intervalo);
+}
+function slide13(){
+    document.getElementById("banner").src="img/S13.JPG"
+    setTimeout("slide14()",intervalo);
+}
+function slide14(){
+    document.getElementById("banner").src="img/S14.JPG"
+    setTimeout("slide15()",intervalo);
+}
+function slide15(){
+    document.getElementById("banner").src="img/S15.JPG"
+    setTimeout("slide16()",intervalo);
+}
+function slide16(){
+    document.getElementById("banner").src="img/S16.JPG"
+    setTimeout("slide17()",intervalo);
+}
+function slide17(){
+    document.getElementById("banner").src="img/S17.JPG"
+    setTimeout("slide18()",intervalo);
+}
+function slide18(){
+    document.getElementById("banner").src="img/S18.JPG"
+    setTimeout("slide1()",intervalo);
+}
